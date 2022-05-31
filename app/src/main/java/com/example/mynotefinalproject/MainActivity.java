fd9d4ac676cd4f49eee251cfdfadc535bef898f9
MyPC <vusongthien1605@gmail.com>
2022-05-31 21:44:24 +0700

    -commit

---
+++ app/src/main/java/com/example/mynotefinalproject/MainActivity.java
@@ -0,0 +1,121 @@
+package com.example.mynotefinalproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.mynotefinalproject.util.JournalApi;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+public class MainActivity extends AppCompatActivity {
+    private Button getStartedButton;
+    private Button loginButton;
+    private Button createAccount;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private FirebaseUser currentUser;
+
+    //Firestore connection
+    private FirebaseFirestore db=FirebaseFirestore.getInstance();
+    private CollectionReference collectionReference=db.collection("Users");
+
+    private EditText emailEditText;
+    private EditText passwordEditText;
+    private ProgressBar progressBar;
+    private Button createAccountButton;
+    private EditText userNameEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getStartedButton=findViewById(R.id.startButton);
+
+        firebaseAuth=FirebaseAuth.getInstance();
+
+        firebaseAuth=FirebaseAuth.getInstance();
+        authStateListener=new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                currentUser=firebaseAuth.getCurrentUser();
+                if(currentUser!=null){
+                    currentUser=firebaseAuth.getCurrentUser();
+                    String currentUserId=currentUser.getUid();
+
+                    collectionReference
+                            .whereEqualTo("userId",currentUserId)
+                            .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                                @Override
+                                public void onEvent(@Nullable QuerySnapshot value,
+                                                    @Nullable FirebaseFirestoreException error) {
+                                    if(error!=null){
+                                        return;
+                                    }
+                                    String name;
+
+                                    if(!value.isEmpty()){
+                                        for(QueryDocumentSnapshot snapshot:value){
+                                            JournalApi journalApi=JournalApi.getInstance();
+                                            journalApi.setUserId(snapshot.getString("userId"));
+                                            journalApi.setUsername(snapshot.getString("username"));
+
+                                            startActivity(new Intent(MainActivity.this,JournalListActivity.class));
+
+                                            finish();
+
+                                        }
+                                    }
+                                }
+                            });
+                }else {
+
+                }
+            }
+        };
+
+        emailEditText=findViewById(R.id.email_account);
+        passwordEditText=findViewById(R.id.password_account);
+        progressBar=findViewById(R.id.login_progress);
+        createAccountButton=findViewById(R.id.create_acct_button);
+        userNameEditText=findViewById(R.id.username_account);
+
+
+        getStartedButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,
+                        LoginActivity.class));
+                finish();
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        currentUser=firebaseAuth.getCurrentUser();
+        firebaseAuth.addAuthStateListener(authStateListener);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(firebaseAuth!=null) {
+            firebaseAuth.removeAuthStateListener(authStateListener);
+        }
+    }
+}
